398836f0ee29ca6bc66ae4fe55ea653190c1bc08
Michele Conway <mconway890@gmail.com>
2018-06-08 14:45:37 -0400

    recipe views, form, routes

--- my_rails_project/app/controllers/recipes_controller.rb
+++ my_rails_project/app/controllers/recipes_controller.rb
@@ -1,2 +1,28 @@
 class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+    if @recipe.save
+      flash[:success] = "Recipe Added!"
+      redirect_to user_recipe_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @recipe = Recipe.find_by(id: params[:id])
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:name, :prep_time, :cook_time)
+  end
 end
--- my_rails_project/config/routes.rb
+++ my_rails_project/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   root to: "home#index"
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    resources :recipes
+  end
+
+  resources :recipes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
