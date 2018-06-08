f7b96b2b7258b560bca070cb35f753eea1c7fe0e
Michele Conway <mconway890@gmail.com>
2018-06-08 14:06:36 -0400

    devise custom fields/registration views edit

--- my_rails_project/config/routes.rb
+++ my_rails_project/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   root to: "home#index"
   resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
