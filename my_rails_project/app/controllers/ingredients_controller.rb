4815bc8165764db28ba48608e83d3588d8d97d23
Michele Conway <mconway890@gmail.com>
2018-06-11 15:27:52 -0400

    ingredient form/routes

--- my_rails_project/app/controllers/ingredients_controller.rb
+++ my_rails_project/app/controllers/ingredients_controller.rb
@@ -1,2 +1,25 @@
 class IngredientsController < ApplicationController
+  def index
+    @ingredients = Ingredient.all
+  end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      flash[:success] = "Ingredient Added!"
+      redirect_to @ingredient
+    else
+      flash[:error] = "Error. Ingredient not added."
+      render 'new'
+    end
+  end
+
+  private
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :quantity)
+  end
 end
--- my_rails_project/config/routes.rb
+++ my_rails_project/config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   end
 
   resources :recipes
-
+  resources :ingredients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
