8e60b0ab7e8f39075f4f298687c7430ad95a698f
Michele Conway <mconway890@gmail.com>
2018-06-12 13:36:22 -0400

    nested routes

--- my_rails_project/config/routes.rb
+++ my_rails_project/config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
     resources :recipes
   end
 
-  resources :recipes
+  resources :recipes do
+    resources :ingredients
+  end
+
   resources :ingredients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
