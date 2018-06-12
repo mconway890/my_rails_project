28b48fb1adb2112e80de560bc908b0a4b4c7a5e2
Michele Conway <mconway890@gmail.com>
2018-06-12 11:53:17 -0400

    validations

--- my_rails_project/app/models/recipe.rb
+++ my_rails_project/app/models/recipe.rb
@@ -2,4 +2,5 @@ class Recipe < ApplicationRecord
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
   belongs_to :user, foreign_key: 'user_id'
+  validates :name, :prep_time, :cook_time, presence: true 
 end
