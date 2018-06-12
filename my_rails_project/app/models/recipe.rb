9d49710a08edd03303c6a9a23f7e9af4ee4b50b0
Michele Conway <mconway890@gmail.com>
2018-06-12 13:41:07 -0400

    nested attributes

--- my_rails_project/app/models/recipe.rb
+++ my_rails_project/app/models/recipe.rb
@@ -2,5 +2,7 @@ class Recipe < ApplicationRecord
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
   belongs_to :user, foreign_key: 'user_id'
-  validates :name, :prep_time, :cook_time, presence: true 
+  validates :name, :prep_time, :cook_time, presence: true
+
+  #accepts_nested_attributes_for :ingredients, :recipe_ingredients
 end
