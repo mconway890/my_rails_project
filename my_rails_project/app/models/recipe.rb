dc7062716a575807b7fb62573b276603e35860bf
Michele Conway <mconway890@gmail.com>
2018-06-12 17:02:36 -0400

    style

--- my_rails_project/app/models/recipe.rb
+++ my_rails_project/app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
-  belongs_to :user, foreign_key: 'user_id'
+  belongs_to :user, :class_name => 'User', :foreign_key => 'user_id'
   validates :name, :prep_time, :cook_time, presence: true
 
   accepts_nested_attributes_for :ingredients, :recipe_ingredients, :allow_destroy => true
--- my_rails_project/app/models/user.rb
+++ my_rails_project/app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :recipes
+  has_many :recipes, :class_name => 'Recipe', :foreign_key => 'user_id'
 end
