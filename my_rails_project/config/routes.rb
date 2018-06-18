10f301b5a7e4327d6601626d30720420bcba12f7
mconway890 <30415200+mconway890@users.noreply.github.com>
2018-06-18 15:33:10 -0400

    routing edit

--- my_rails_project/config/routes.rb
+++ my_rails_project/config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
     resources :recipes
   end
 
-  resources :recipes do
-    resources :ingredients
-  end
+  #resources :recipes do
+  #  resources :ingredients
+  #end
 
   resources :ingredients
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
