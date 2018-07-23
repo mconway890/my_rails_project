f9d9e2f80fd92d5da2ce42c0c2b916256e484119
Michele Conway <mconway890@gmail.com>
2018-07-23 14:14:52 -0400

    recipe form edit, styling

--- my_rails_project/app/models/ingredient.rb
+++ my_rails_project/app/models/ingredient.rb
@@ -2,4 +2,8 @@ class Ingredient < ApplicationRecord
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
   validates :name, presence: true
+
+  def name_for_select
+    name.capitalize
+  end
 end
