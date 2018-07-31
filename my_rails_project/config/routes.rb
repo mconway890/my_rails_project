bbf8b63dedf87c777f2bab19993508cd6a63766d
Michele Conway <mconway890@gmail.com>
2018-07-31 14:06:27 -0400

    nested reviews in user

--- my_rails_project/config/routes.rb
+++ my_rails_project/config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show, :edit, :update] do
     resources :recipes
+      resources :reviews, only: [:index, :show]
   end
 
   resources :recipes do
