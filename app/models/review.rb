c5cbaafece5d99b00451f966b80cb4e22403dcd2
Michele Conway <mconway890@gmail.com>
2018-09-11 15:58:56 -0400

    validate presence of reviewer

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,5 @@ class Review < ApplicationRecord
   belongs_to :recipe
   belongs_to :user
   validates :difficulty, numericality: { only_integer: true }
+  validates :reviewer, :presence => true
 end
