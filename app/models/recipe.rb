20176195d9d8db72fbb2453392607ae22e34c3ab
Michele Conway <mconway890@gmail.com>
2018-09-11 15:31:40 -0400

    ingredient presence validation in recipe form

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,6 +5,7 @@ class Recipe < ApplicationRecord
   belongs_to :user, :class_name => 'User', :foreign_key => 'user_id'
   validates :name, :prep_time, :cook_time, :instructions, presence: true
   validates :prep_time, :cook_time, numericality: { only_integer: true }
+  validates :ingredients, presence: true
 
   scope :quickest, -> { where('cook_time + prep_time < ?', 20) }
 
