96a9879a0c3b3e3db0d46dd5d07a43855983daef
Michele Conway <mconway890@gmail.com>
2018-09-11 15:14:42 -0400

    add ingredient uniqueness, avoid duplication

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,7 +1,7 @@
 class Ingredient < ApplicationRecord
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
-  validates :name, presence: true
+  validates :name, :presence => true, :uniqueness => true
 
   def name_for_select
     name.downcase
