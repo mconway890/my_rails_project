2a54d5d8bd5a8c283d762d2df76bae18afebf290
Michele Conway <mconway890@gmail.com>
2018-09-26 14:46:42 -0400

    serialize method

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,4 +1,16 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :name, :instructions, :prep_time, :cook_time
+  attributes :id, :name, :instructions, :prep_time, :cook_time, :user_id
   belongs_to :user
+
+  def self.serialize(recipe)
+    serialized_recipe = '{'
+    serialized_recipe += '"id": ' + recipe.id.to_s + ', '
+    serialized_recipe += '"name": "' + recipe.name + '", '
+    serialized_recipe += '"prep_time": "' + recipe.prep_time.to_s + '", '
+    serialized_recipe += '"cook_time": "' + recipe.cook_time.to_s + '", '
+    serialized_recipe += '"instructions": "' + recipe.instructions + '", '
+    # serialized_recipe += '"user": {'
+    # serialized_recipe += '"email": "' + recipe.user.email + '"}'
+    serialized_recipe += '}'
+  end
 end
