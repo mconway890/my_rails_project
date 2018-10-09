dd55283791e28e08c5c62788fb7341fa13f252fd
Michele Conway <mconway890@gmail.com>
2018-10-09 10:21:27 -0400

    add comments to js files

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -1,16 +1,20 @@
 $(function(){
+  //Listen for submission of the form
   $("form").submit(function(e){
-    e.preventDefault();
+    //Get action and method from form itself - this
     var action = $(this).attr('action');
     var method = $(this).attr('method');
+    //Store form values in variables to be passed in Ajax request
     var recipe_name = $(this).find('#recipe_name').val();
+    //JQuery method to turn form data into object we can use in Ajax call
     var data = $(this).serializeArray();
-
+    //Make ajax request
     $.ajax({
       method: method,
       url: action,
       data: data,
       dataType: 'script'
     })
+    e.preventDefault();
   })
 })
--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,21 +1,23 @@
 $(function(){
+  //Listen for submission of the form
   $("form").submit(function(e){
-    e.preventDefault();
-
+    //Get action and method from form itself - this
     var action = $(this).attr('action');
     var method = $(this).attr('method');
-
+    //Store form values in variables to be passed in Ajax request
     var description = $(this).find('#review_description').val();
     var reviewer = $(this).find('#review_reviewer').val();
-
+    //JQuery method to turn form data into object we can use in Ajax call
     var data = $(this).serializeArray();
+    //Make ajax request
     $.ajax({
       method: method,
       url: action,
       data: data,
       dataType: 'script'
-
+      //Prevent duplicate form submissions
       return false;
     })
+    e.preventDefault();
   })
 })
