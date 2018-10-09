da15ce7dfa1c94bbd1d825eb9785fa278cab3933
Michele Conway <mconway890@gmail.com>
2018-10-09 14:18:22 -0400

    render quick recipes show using jquery

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -1,14 +1,14 @@
-$(function(){
+$(function() {
   //Listen for submission of the form
-  $("form").submit(function(e){
+  $("form").submit(function(e) {
     e.preventDefault();
     //Get action and method from form itself - this
-    var action = $(this).attr('action');
-    var method = $(this).attr('method');
-    //Store form values in variables to be passed in Ajax request
-    var recipe_name = $(this).find('#recipe_name').val();
+    let action = $(this).attr('action');
+    let method = $(this).attr('method');
+    //Store form values in letiables to be passed in Ajax request
+    let recipe_name = $(this).find('#recipe_name').val();
     //JQuery method to turn form data into object we can use in Ajax call
-    var data = $(this).serializeArray();
+    let data = $(this).serializeArray();
     //Make ajax request
     $.ajax({
       method: method,
@@ -18,3 +18,14 @@ $(function(){
     })
   })
 })
+
+
+function timeSavers() {
+  $('#quick').load('quickest.html #quick')
+  let e = document.getElementById('quick');
+  if (e.style.display != 'none') {
+    e.style.display = 'none';
+  } else {
+    e.style.display = '';
+  }
+}
