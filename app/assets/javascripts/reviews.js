026adf1fc607b0f848c1ba03dde26be1148ea3b7
Michele Conway <mconway890@gmail.com>
2018-10-15 13:26:12 -0400

    review protoype with method

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,6 +1,6 @@
 $(function(){
   //Listen for submission of the form
-  $("form").submit(function(e){
+  $("#review_form").submit(function(e){
     e.preventDefault();
     //Get action and method from form itself - this
     let action = $(this).attr('action');
@@ -8,6 +8,7 @@ $(function(){
     //Store form values in variables to be passed in Ajax request
     let description = $(this).find('#review_description').val();
     let reviewer = $(this).find('#review_reviewer').val();
+    let difficulty = $(this).find('#review_difficulty').val();
     //JQuery method to turn form data into object we can use in Ajax call
     let data = $(this).serializeArray();
     //Make ajax request
@@ -23,15 +24,15 @@ $(function(){
 })
 
 // Js model object with method added to prototype
-// function Review(reviewer, description, difficulty) {
-//   this.reviewer = reviewer;
-//   this.description = description;
-//   this.difficulty = difficulty;
-// }
-//
-// Review.prototype.fullReview = function() {
-//   console.log(`Review by: ${this.reviewer} | Description: ${this.description} | Difficulty: ${this.difficulty}`);
-// }
+function Review(reviewer, description, difficulty) {
+  this.reviewer = reviewer;
+  this.description = description;
+  this.difficulty = difficulty;
+}
+
+Review.prototype.fullReview = function() {
+  console.log(`Review by: ${this.reviewer} | Description: ${this.description} | Difficulty: ${this.difficulty}`);
+}
 //
 // let m = new Review('Michele','delicious!', 2);
 // m.fullReview();
