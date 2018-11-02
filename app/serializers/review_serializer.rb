53561f4565cfd71938eb7f5e3f913a0cba7412a5
Michele Conway <mconway890@gmail.com>
2018-11-02 11:26:11 -0400

    add attributes to review serializer

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,6 +1,6 @@
 class ReviewSerializer < ActiveModel::Serializer
   # attributes returned in JSON response
-  attributes :id, :description, :user_id
+  attributes :id, :description, :user_id, :recipe_id, :difficulty
   belongs_to :recipe
   belongs_to :user
 end
